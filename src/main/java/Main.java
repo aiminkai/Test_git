d17a82a096cab6198d60580958ddc464d9e78ff5
aiminkai <m.a.azarka@gmail.com>
2022-11-06 13:23:55 +0300

    branch merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("first");
+        System.out.println("branch");
         System.out.println("second");
         System.out.println("bugFix");
     }
